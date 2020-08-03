80408a70564dcb2b4e096dff0d68793b3d8fe6ad
Pedro <pedropaulo.azevedo@hotmail.com.br>
2020-08-03 17:12:42 -0300

    Generator boss added

---
+++ Assets/Scripts/GeradorChefeZumbi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorChefeZumbi : MonoBehaviour
+{
+    public GameObject ChefePrefab;
+    private float _tempoMaximoGeracao = 20;
+    private float _tempoParaProximaGeracao;
+
+    void Start()
+    {
+        _tempoParaProximaGeracao = _tempoMaximoGeracao;
+    }
+
+    void Update()
+    {
+        if(Time.timeSinceLevelLoad >= _tempoParaProximaGeracao)
+        {
+            Instantiate(ChefePrefab, transform.position, Quaternion.identity);
+            _tempoParaProximaGeracao = Time.timeSinceLevelLoad + _tempoMaximoGeracao;
+        }
+    }
+}
